cfa2862b3bb4e00dbb116adb5ccaf5967f39d92d
vikash <vikashkumar04081999@gmail.com>
2023-10-17 10:17:27 +0530

    pushed v 1.0.3

--- official/src/components/Navbar/Navbar.jsx
+++ official/src/components/Navbar/Navbar.jsx
@@ -1,14 +1,34 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Link, Outlet } from "react-router-dom";
 import "./Navbar.css";
 import logo from "../../assets/logo.png";
 import { TbPower } from "react-icons/tb";
 
 const Navbar = ({ setAuth }) => {
+  const [loggedIn, setLoggedIn] = useState(false);
+
   const handleLogout = () => {
     sessionStorage.removeItem("token");
     setAuth(false);
   };
+
+  const verifytoken = async (token) => {
+    const user = await window.api.verifyToken(token);
+    return user;
+  };
+
+  const token = sessionStorage.getItem("token");
+
+  useEffect(() => {
+    const userAuth = async () => {
+      const user = await verifytoken(token);
+      if (user) {
+        setLoggedIn(true);
+      }
+    };
+    userAuth();
+  }, [token, setAuth]);
+
   return (
     <nav className="nav">
       <div className="nav-container">
@@ -20,7 +40,7 @@ const Navbar = ({ setAuth }) => {
             Dashboard
           </Link>
           <div className="nav-user">
-            <TbPower onClick={handleLogout} />
+            {loggedIn ? <TbPower onClick={handleLogout} /> : ""}
           </div>
           <Outlet />
         </div>
